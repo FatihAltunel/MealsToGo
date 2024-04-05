b39da3a52941cdd6a037a5914453c06a7436b0a9
Fatih Altunel <jjrousseau1128@gmail.com>
2024-04-05 16:39:48 +0300

    Some code cleaning

--- src/features/restaurans/components/Restaurants-Info-card.component.js
+++ src/features/restaurans/components/Restaurants-Info-card.component.js
@@ -1,59 +1,22 @@
 import React from "react";
-import { Text, Card, View } from "react-native-paper";
-import styled from "styled-components";
+import { Text } from "react-native-paper";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../../infrastructure/theme/index";
 import { SvgXml } from 'react-native-svg';
 import star from '../../../../assets/star';
 import open from '../../../../assets/open';
+import { 
+    RestaurantCard,
+    RestaurantCardCover,
+    Info,
+    Stars,
+    Rating,
+    Section,
+    SectionEnd,
+    RestaurantCardAddress,
+    RestaurantCardTitle
+} from "./Restaurants-info-card.styles";
 
-    const RestaurantCard = styled(Card)` 
-    background-color: ${(props)=> props.theme.colors.bg.primary};
-    `;
-
-    const RestaurantCardCover = styled(Card.Cover)`
-    padding: ${(props) => props.theme.space[2]};
-    background-color: ${(props)=> props.theme.colors.bg.primary};
-    `;
-
-    const Info = styled.View`
-    padding: ${(props) => props.theme.space[3]};
-    `;
-
-    const Stars = styled(SvgXml)``;
-
-    const Rating = styled.View`
-    flex-direction:row;
-    padding-top: ${(props) => props.theme.space[2]};
-    padding-bottom: ${(props) => props.theme.space[1]};
-    `;
-
-    const Section = styled.View`
-    flex-direction:row;
-    align-items: center;
-    `;
-
-    const SectionEnd = styled.View`
-    flex-direction:row;
-    flex:1;
-    justify-content: flex-end;
-    `;
-
-    const Open = styled(SvgXml)`
-    flex-direction:row;
-    `;
-
-    const RestaurantCardAddress = styled(Text)`
-    font-family: ${(props)=>props.theme.fonts.body};
-    font-size:${(props) => props.theme.fontSizes.caption};
-    color: ${(props)=>props.theme.colors.ui.primary};
-    `;
-
-    const RestaurantCardTitle = styled(Text)`
-    font-family: ${(props)=>props.theme.fonts.heading};
-    font-size:${(props) => props.theme.fontSizes.body};
-    color: ${(props)=>props.theme.colors.ui.primary};
-    `;
     
 export const RestaurantsInfoCard = ({ Restaurants = {} }) => {
     const {
---
+++ src/features/restaurans/components/Restaurants-info-card.styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import { Text } from "react-native-paper";
+
+
+    
+    export const RestaurantCard = styled(Card)` 
+    background-color: ${(props)=> props.theme.colors.bg.primary};
+    `;
+    
+    export const RestaurantCardCover = styled(Card.Cover)`
+    padding: ${(props) => props.theme.space[2]};
+    background-color: ${(props)=> props.theme.colors.bg.primary};
+    `;
+    
+    export const Info = styled.View`
+    padding: ${(props) => props.theme.space[3]};
+    `;
+    
+    export const Stars = styled(SvgXml)``;
+    
+    export const Rating = styled.View`
+    flex-direction:row;
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[1]};
+    `;
+    
+    export const Section = styled.View`
+    flex-direction:row;
+    align-items: center;
+    `;
+
+    export const SectionEnd = styled.View`
+    flex-direction:row;
+    flex:1;
+    justify-content: flex-end;
+    `;
+    
+    export const RestaurantCardAddress = styled(Text)`
+    font-family: ${(props)=>props.theme.fonts.body};
+    font-size:${(props) => props.theme.fontSizes.caption};
+    color: ${(props)=>props.theme.colors.ui.primary};
+    `;
+    
+    export const RestaurantCardTitle = styled(Text)`
+    font-family: ${(props)=>props.theme.fonts.heading};
+    font-size:${(props) => props.theme.fontSizes.body};
+    color: ${(props)=>props.theme.colors.ui.primary};
+    `;
